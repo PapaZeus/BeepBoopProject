f55b4364900071179cce04dfec63a174ef9e9411
Guest User <Guest@epicodus-118.local>
2019-04-05 13:52:12 -0700

    Add for loop with if and else if statements that will return results using match method matching userInputs of 1,2,3

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,29 @@ $(document).ready(function() {
   });
 
 
-
-
-
-
 //BACK END
 
 function beepboopGenerator(userInput) {
 
 let results = [];
 
-}
+  for (let i = 1; i <= userInput; i++) {
+
+    if (i.toString().match(/3/)) {
+
+      results.push(" I'm sorry Dave. I'm afraid I can't do that. ")
+    }
+    else if (i.toString().match(/1/)) {
+
+      results.push(" Beep! ")
+    }
+    else if (i.toString().match(/2/)) {
+
+      results.push(" Boop! ")
+    }
+    
+    }
+    return results;
+
+  }
+});
